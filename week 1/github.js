2f138bfd3e05239365d8c18f3a69a88ff58062df
sklem <stephenpklem@gmail.com>
2019-02-20 20:50:33 -0500

    Add existing file

--- week 1/github.js
+++ week 1/github.js
@@ -3,29 +3,34 @@ Exercise File for Week 1 Source, see Week1.txt for details
 */
 
 /*
-These are functions, but why the weird syntax?
+These are functions, but why the weird syntax? The functions intake a particular list as a parameter.
 */
+
 const personOne = favoriteFoods => {
-  /*insert functionality here*/
+  return favoriteFoods[Math.floor(Math.random()*favoriteFoods.length)];
 };
 
 const personTwo = favoriteMovies => {
-  /*insert functionality here*/
+  return favoriteMovies[Math.floor(Math.random()*favoriteMovies.length)];
 };
 
 const personThree = favoriteAnimals => {
-  /*insert functionality here*/
+  return favoriteAnimals[Math.floor(Math.random()*favoriteAnimals.length)];
 };
 
 /*
-What does this variable syntax do?
+What does this variable syntax do? Classifies the constants as a list of any data type of elements.
 */
-const favoriteFoods = [],
-  favoriteMovies = [],
-  favoriteAnimals = [];
+const favoriteFoods = ["spaghetti", "peas", "nectarines", "donuts", "steak"],
+  favoriteMovies = ["Napoleon Dynamite", "School of Rock", "Nacho Libre", "Skyfall", "The Dark Knight"],
+  favoriteAnimals = ["the Labrador Retriever Dog", "the Squirrel", "the Flying Ant", "the Red Tailed Hawk", "the Silverback Gorilla"];
 
 const iceBreaker = () => {
-  /*
-    Call each function here
-    */
+  let food = personOne(favoriteFoods);
+  let movie = personTwo(favoriteMovies);
+  let animal = personThree(favoriteAnimals);
+  console.log("A particular food that I like is " + food + ".");
+  console.log("A particular movie that I like is " + movie + ".");
+  console.log("A particular animal that I like is " + animal + ".");
 };
+iceBreaker();
